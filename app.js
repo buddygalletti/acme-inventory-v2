0f56c0b65736d41ae75cf234225d3b182862d796
Buddy Galletti <buddygalletti@gmail.com>
2019-06-25 08:13:41 -0700

    FINALLY got the update working.

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const Sequelize = require('sequelize');
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const { db, Product, syncAndSeed } = require('./server');
+const Sequelize = require("sequelize");
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const { db, Product, syncAndSeed } = require("./server");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get('/api/products', async (req, res, next) => {
+app.get("/api/products", async (req, res, next) => {
   try {
     const products = await Product.findAll();
     res.send(products);
@@ -23,10 +23,10 @@ app.get('/api/products', async (req, res, next) => {
   }
 });
 
-app.put('/api/products/:id', async (req, res, next) => {
+app.put("/api/products/:id", async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.id * 1);
-    const status = await product.status;
+    const status = await req.body.status;
     await product.update({ status });
     res.status(201).send(product);
   } catch (ex) {
