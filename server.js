09665c337c166872e683969ecca8a980830692ac
Buddy Galletti <buddygalletti@gmail.com>
2019-06-25 08:33:45 -0700

    Deploying on heroku

--- server.js
+++ server.js
@@ -1,37 +1,51 @@
-const Sequelize = require('sequelize');
-const express = require('express');
+const Sequelize = require("sequelize");
+const express = require("express");
 
 const db = new Sequelize(`postgres://localhost:5432/acme_inventory`, {
   logging: false
 });
 
-const Product = db.define('product', {
+const Product = db.define("product", {
   name: {
     type: Sequelize.STRING,
     allowNull: false
   },
   status: {
-    type: Sequelize.ENUM('INSTOCK', 'BACKORDERED', 'DISCONTINUED'),
-    defaultValue: 'INSTOCK'
+    type: Sequelize.ENUM("INSTOCK", "BACKORDERED", "DISCONTINUED"),
+    defaultValue: "INSTOCK"
   }
 });
 
 // need to write a syncAndSeed()
 sampleProducts = [
   {
-    name: 'prod1',
-    status: 'BACKORDERED'
+    name: "prod1",
+    status: "BACKORDERED"
   },
   {
-    name: 'prod2'
+    name: "prod2"
   },
   {
-    name: 'prod3',
-    status: 'DISCONTINUED'
+    name: "prod3",
+    status: "DISCONTINUED"
   },
   {
-    name: 'prod4',
-    status: 'BACKORDERED'
+    name: "prod4",
+    status: "BACKORDERED"
+  },
+  {
+    name: "prod5",
+    status: "BACKORDERED"
+  },
+  {
+    name: "prod6"
+  },
+  {
+    name: "prod7",
+    status: "DISCONTINUED"
+  },
+  {
+    name: "prod8"
   }
 ];
 
