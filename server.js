9465a88e68425333c9b8bcdae8bbe7376705b8d8
Buddy Galletti <buddygalletti@gmail.com>
2019-06-25 12:33:10 -0700

    Changed the file names

--- server.js
+++ server.js
@@ -1,71 +1,38 @@
-const Sequelize = require("sequelize");
-const express = require("express");
+const Sequelize = require('sequelize');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { db, Product, syncAndSeed } = require('./database');
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/acme_inventory"
-);
+const app = express();
 
-const Product = db.define("product", {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  status: {
-    type: Sequelize.ENUM("INSTOCK", "BACKORDERED", "DISCONTINUED"),
-    defaultValue: "INSTOCK"
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/api/products', async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.send(products);
+  } catch (ex) {
+    next(ex);
   }
 });
 
-// need to write a syncAndSeed()
-sampleProducts = [
-  {
-    name: "prod1",
-    status: "BACKORDERED"
-  },
-  {
-    name: "prod2"
-  },
-  {
-    name: "prod3",
-    status: "DISCONTINUED"
-  },
-  {
-    name: "prod4",
-    status: "BACKORDERED"
-  },
-  {
-    name: "prod5",
-    status: "BACKORDERED"
-  },
-  {
-    name: "prod6"
-  },
-  {
-    name: "prod7",
-    status: "DISCONTINUED"
-  },
-  {
-    name: "prod8"
-  }
-];
-
-const syncAndSeed = async () => {
+app.put('/api/products/:id', async (req, res, next) => {
   try {
-    await db.sync({ force: true });
-    await Promise.all(
-      sampleProducts.map(product => {
-        return Product.create(product);
-      })
-    );
+    const product = await Product.findByPk(req.params.id * 1);
+    const status = await req.body.status;
+    await product.update({ status });
+    res.status(201).send(product);
   } catch (ex) {
-    console.log(ex);
+    next(ex);
   }
-};
+});
 
-syncAndSeed();
-
-module.exports = {
-  db,
-  Product,
-  syncAndSeed
-};
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port: ${port}`));
