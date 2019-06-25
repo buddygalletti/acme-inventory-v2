fcfcbc5aac3d1808f33c7f55966b210dc7ffedb6
Buddy Galletti <buddygalletti@gmail.com>
2019-06-25 14:15:18 -0700

    Forgot a try catch

--- database.js
+++ database.js
@@ -1,51 +1,51 @@
-const Sequelize = require("sequelize");
-const express = require("express");
+const Sequelize = require('sequelize');
+const express = require('express');
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/acme_inventory"
+  process.env.DATABASE_URL || 'postgres://localhost/acme_inventory'
 );
 
-const Product = db.define("product", {
+const Product = db.define('product', {
   name: {
     type: Sequelize.STRING,
     allowNull: false
   },
   status: {
-    type: Sequelize.ENUM("INSTOCK", "BACKORDERED", "DISCONTINUED"),
-    defaultValue: "INSTOCK"
+    type: Sequelize.ENUM('INSTOCK', 'BACKORDERED', 'DISCONTINUED'),
+    defaultValue: 'INSTOCK'
   }
 });
 
 // need to write a syncAndSeed()
 sampleProducts = [
   {
-    name: "prod1",
-    status: "BACKORDERED"
+    name: 'prod1',
+    status: 'BACKORDERED'
   },
   {
-    name: "prod2"
+    name: 'prod2'
   },
   {
-    name: "prod3",
-    status: "DISCONTINUED"
+    name: 'prod3',
+    status: 'DISCONTINUED'
   },
   {
-    name: "prod4",
-    status: "BACKORDERED"
+    name: 'prod4',
+    status: 'BACKORDERED'
   },
   {
-    name: "prod5",
-    status: "BACKORDERED"
+    name: 'prod5',
+    status: 'BACKORDERED'
   },
   {
-    name: "prod6"
+    name: 'prod6'
   },
   {
-    name: "prod7",
-    status: "DISCONTINUED"
+    name: 'prod7',
+    status: 'DISCONTINUED'
   },
   {
-    name: "prod8"
+    name: 'prod8'
   }
 ];
 
